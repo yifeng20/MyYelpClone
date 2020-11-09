9f0077427226d6404831276f50c604b0cccb3da3
yifeng20 <yifeng20@colby.edu>
2020-11-09 10:31:21 +0800

    Initial commitv -- create project

---
+++ app/src/main/java/edu/stanford/yifeng98/myyelpclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.stanford.yifeng98.myyelpclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
